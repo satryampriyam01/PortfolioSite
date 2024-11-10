056d83fb1801f78c3b6872c663c5a595c8e8f1db
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-10 02:42:39 -0500

    Update page.jsx
    Menucha

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,13 +22,13 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Hola! I'm Satyam Priyam!  
+            Hola! I'm Satyam Priyam WHO LOVES COCO SO MUCH Thats why he is smiling here! View on desktop not on iPhone!
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Welcome to my digital canvas, where technology meets innovation.
-           I am a dynamic Software Development Engineer with a strong foundation in Full Stack Development, Machine Learning, and Product Management.
-            My portfolio reflects my commitment to pushing the boundaries of what's possible.
+          Hey love, you know, I’ve been thinking... if we were a Wi-Fi network, you’d be the password because without you, 
+            I’m completely lost. I might be in Boston, but every time you smile, I feel like I’m in the middle of a cloud of happiness (no, not the tech cloud, the one that’s way better!). You’re my favorite notification, and every time you text, it’s like my phone’s battery instantly gets a 100% charge—thank you for being my constant source of energy and joy. 
+            Also, if you were a software update, I’d never hit ‘remind me later.’ You’ve already updated my heart to the latest version!
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
