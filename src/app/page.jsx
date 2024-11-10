538956ca153e1b048844e1bfb6d6e9e0604dd79f
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-10 10:10:59 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/Menuchaandsatyam.png" alt="" fill className="object-contain" />
+          <Image src="/file_1.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
@@ -26,9 +26,9 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Hey love, you know, I’ve been thinking... if we were a Wi-Fi network, you’d be the password because without you, 
-            I’m completely lost. I might be in Boston, but every time you smile, I feel like I’m in the middle of a cloud of happiness (no, not the tech cloud, the one that’s way better!). You’re my favorite notification, and every time you text, it’s like my phone’s battery instantly gets a 100% charge—thank you for being my constant source of energy and joy. 
-            Also, if you were a software update, I’d never hit ‘remind me later.’ You’ve already updated my heart to the latest version!
+          Welcome to my digital canvas, where technology meets innovation. 
+            I am a dynamic Software Development Engineer with a strong foundation in Full Stack Development, Machine Learning, and Product Management. 
+            My portfolio reflects my commitment to pushing the boundaries of what's possible.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
