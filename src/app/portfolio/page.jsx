b4c14b90b42930796d0d23dca51b28f83e4d4234
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-10 10:13:47 -0500

    Update page.jsx

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -126,7 +126,7 @@ const PortfolioPage = () => {
             href="/contact"
             className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
           >
-            Hire Me
+            Contact Me
           </Link>
         </div>
       </div>
