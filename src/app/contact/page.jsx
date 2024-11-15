5baedce6c01c211bb473584b6c3cbbdc77c0eca6
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-15 15:05:21 -0500

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -86,7 +86,7 @@ const ContactPage = () => {
     </a>
   </h1>
   <h1 className="text-xl font-bold text-white-700 mt-4 mb-1">Phone:</h1>
-  <h1 className="text-lg text-yellow-700">
+  <h1 className="text-lg text-yellow-500">
     <a href="tel:+18573437331" className="text-yellow-700 hover:underline">
       857-343-7331
     </a>
